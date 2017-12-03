9b2138b5f77f49eabebcb2c4e5d02ba82b8157fc
Jemmy Zhang <hzdan360@163.com>
2017-12-03 16:54:24 +0800

    add git ignore add pom.xml
    Signed-off-by: Jemmy Zhang <hzdan360@163.com>

---
+++ simple-eureka-server/src/main/java/pers/jz/eureka/Application.java
@@ -0,0 +1,10 @@
+package pers.jz.eureka;
+
+/**
+ * @author jemmyzhang on 2017/12/3.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
